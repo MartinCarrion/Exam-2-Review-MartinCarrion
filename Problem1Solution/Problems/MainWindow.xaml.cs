4adaeb29e980ad3464e4e9330ae227cd76a5df52
MartinCarrion <martin.a.carrion-1@ou.edu>
2019-03-30 18:41:59 -0500

    Begin Problem 1

---
+++ Problem1Solution/Problems/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿//Using the Employees.txt file, read in the file and show a list of all the employees that make
+//over 70,000 in salary.In a separate list, display all of the employees that have an email
+//address at dropbox.com.When reading in the file, save the values in a class that has
+//constructors as well as an overridden ToString method.Output the average salary for all of
+//the employees as well.
+//a.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Problems
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
